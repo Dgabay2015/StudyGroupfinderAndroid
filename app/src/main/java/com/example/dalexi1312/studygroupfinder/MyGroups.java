c0fcb35a12a021539d555cb78ec3b920ab3ebe8a
Dgabay2015 <dgabay2015@fau.edu>
2016-12-09 15:30:41 -0500

    made circular  sign in butt + added listview of mygroups

---
+++ app/src/main/java/com/example/dalexi1312/studygroupfinder/MyGroups.java
@@ -0,0 +1,13 @@
+package com.example.dalexi1312.studygroupfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyGroups extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_groups);
+    }
+}
