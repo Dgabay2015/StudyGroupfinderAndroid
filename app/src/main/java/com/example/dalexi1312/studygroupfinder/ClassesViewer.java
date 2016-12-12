d28ce3348b763b7b65f66fa57cacf486d0864fdb
Dgabay2015 <dgabay2015@fau.edu>
2016-12-12 00:43:23 -0500

    Added function where you choose a class from spinner then on the next page you get that class option

--- app/src/main/java/com/example/dalexi1312/studygroupfinder/ClassesViewer.java
+++ app/src/main/java/com/example/dalexi1312/studygroupfinder/ClassesViewer.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -12,6 +13,8 @@ public class ClassesViewer extends AppCompatActivity {
     private Button mClassesFoundButton;
     private Spinner mSpinner;
     ArrayAdapter<CharSequence> mAdapter;
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,20 @@ public class ClassesViewer extends AppCompatActivity {
         mAdapter=ArrayAdapter.createFromResource(this,R.array.course_names,android.R.layout.simple_spinner_item);
         mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSpinner.setAdapter(mAdapter);
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //if item in spinner select here you would do something
+                message= parent.getItemAtPosition(position)+" class";
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
 
 
@@ -36,6 +53,7 @@ public class ClassesViewer extends AppCompatActivity {
     }
     private void goToClassView() {
         Intent intent = new Intent(this, classview.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/dalexi1312/studygroupfinder/classview.java
+++ app/src/main/java/com/example/dalexi1312/studygroupfinder/classview.java
@@ -8,13 +8,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class classview extends AppCompatActivity {
     private FloatingActionButton maddGroup;
+    private TextView mTextViewClassName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classview);
+
+        mTextViewClassName = (TextView)findViewById(R.id.showClassNameTextView) ;
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(ClassesViewer.EXTRA_MESSAGE);
+        mTextViewClassName.setText(message);
+
+
         maddGroup=(FloatingActionButton)findViewById(R.id.floatingActionButtonAddGroup);
         maddGroup.setOnClickListener(new View.OnClickListener() {
             @Override
