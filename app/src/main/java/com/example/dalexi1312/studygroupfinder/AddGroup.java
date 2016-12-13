ab75bf9b31d7d55e9867fcb1c888e7de97d11402
Dgabay2015 <dgabay2015@fau.edu>
2016-12-12 19:50:25 -0500

    Commented all facebook code out

---
+++ app/src/main/java/com/example/dalexi1312/studygroupfinder/AddGroup.java
@@ -0,0 +1,61 @@
+package com.example.dalexi1312.studygroupfinder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class AddGroup extends AppCompatActivity {
+   private Spinner mSpinner;
+    ArrayAdapter<CharSequence> mAdapter;
+    private Button mAddGroupButton;
+    public String message;
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_group);
+        mSpinner=(Spinner)findViewById(R.id.spinnerClasses);
+        mAdapter=ArrayAdapter.createFromResource(this,R.array.course_names,android.R.layout.simple_spinner_item);
+        mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(mAdapter);
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+               Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is the best class!!",Toast.LENGTH_SHORT).show();
+                message = parent.getItemAtPosition(position)+"";
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        mAddGroupButton= (Button)findViewById(R.id.buttonSubmit);
+        mAddGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToGroupView();
+            }
+        });
+
+
+    }
+    private void goToGroupView()
+    {
+        Intent intent = new Intent(this,GroupPage.class);
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+    }
+
+}
